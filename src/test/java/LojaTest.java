c921746e59e324c4eeaef8e553071958178e1dec
Anderson Guerra <and.guerra@outlook.com>
2021-11-29 09:43:04 -0300

    implementação de alguns teste

---
+++ src/test/java/LojaTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LojaTest {
+
+    @Test
+    void addProduto() {
+        Loja l = new Loja();
+        Produto p = new Produto("Nome", 10, 10);
+        // VERIFICA SE O PRODUTO NÃO É NULO
+        assertNotNull(p);
+        l.addProduto(p);
+        // VERIFICA SE A LOJA TEM 1 PRODUTO
+        assertEquals(1, l.Quantidade());
+    }
+
+    @Test
+    void removeProduto() {
+        Loja l = new Loja();
+        Produto p = new Produto("Nome", 10, 10);
+        // VERIFICA SE O PRODUTO NÃO É NULO
+        assertNotNull(p);
+        l.addProduto(p);
+        // VERIFICA SE A LOJA TEM 1 PRODUTO
+        assertEquals(1, l.Quantidade());
+        l.RemoveProduto("Nome");
+        // VERIFICA SE A LOJA TEM 0 PRODUTOS
+        assertEquals(0, l.Quantidade());
+    }
+
+    @Test
+    void mostraProdutos() {
+        Loja l = new Loja();
+        Produto p = new Produto("Produto 1", 10, 15);
+        Produto p2 = new Produto("Produto 2", 32, 178);
+        l.addProduto(p);
+        l.addProduto(p2);
+        // VERIFICA SE A LOJA TEM 2 PRODUTOS
+        assertEquals(2, l.Quantidade());
+    }
+
+    @Test
+    void quantidade() {
+    }
+
+    @Test
+    void busca() {
+        Loja l = new Loja();
+        Produto p = new Produto("Produto 1", 10, 15);
+        l.addProduto(p);
+        // VERIFICA SE ACHOU O PRODUTO
+        assertEquals(true, l.Busca("Produto 1"));
+    }
+
+    @Test
+    void listaTudo() {
+    }
+}
